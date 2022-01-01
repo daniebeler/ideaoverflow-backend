50fca83688fe78c03e2d0b16d1554130506cabed
daniebeler <hiebeler.daniel@gmail.com>
2022-01-01 14:30:58 +0100

    fixed post submit

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -6,14 +6,30 @@ router.post('/create', async (req, res) => {
   database.getConnection((_err, con) => {
     con.query(`
     INSERT INTO posts (OwnerUserId, Title, Body)
-    VALUES (${con.escape(req.body.userId)}, ${con.escape(req.body.title)}, ${con.escape(req.body.body)})
+    VALUES (${con.escape(req.body.userID)}, ${con.escape(req.body.header)}, ${con.escape(req.body.body)})
     `, (err, user) => {
       if (err) {
         con.release()
         return res.status(500).json({ err })
       } else {
         con.release()
-        return res.json({ status: 200, header: 'Juhuu', message: 'Stonks' }).send()
+        return res.status(200).json({ status: 200, header: 'Juhuu', message: 'Stonks' })
+      }
+    })
+  })
+})
+
+router.get('/latest', (req, res) => {
+  database.getConnection((_err, con) => {
+    con.query('SELECT * FROM posts', (err, result) => {
+      if (err) {
+        return res.status(500).json({ err })
+      } else {
+        if (result[0]) {
+          return res.send(result)
+        } else {
+          return res.status(500).json({ message: 'user nicht gefunden' })
+        }
       }
     })
   })
