58cb9126cd3a687c33eb45e30d54c70c4d2c84dd
daniebeler <hiebeler.daniel@gmail.com>
2022-01-16 13:48:24 +0100

    implemented saved posts

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -35,6 +35,8 @@ router.get('/numberoftotalposts', (req, res) => {
 router.post('/posts', (req, res) => {
   let usernameFilter = ''
   let searchFilter = ''
+  let savedFilter = ''
+  let order = 'ORDER BY p.id'
 
   if (req.body.username) {
     usernameFilter = 'WHERE u.username = "' + req.body.username + '"'
@@ -44,19 +46,44 @@ router.post('/posts', (req, res) => {
     searchFilter = 'WHERE u.username LIKE "' + req.body.searchTerm + '"'
   }
 
+  if (req.body.savedByUsername) {
+    savedFilter = 'INNER JOIN user_saves_post s ON s.post_id = p.id WHERE s.user_id = ' + req.body.currentUserId
+    order = 'ORDER BY s.savedAt DESC'
+  }
+
   database.getConnection((_err, con) => {
     con.query(`
       SELECT p.*, u.*, vv.value as votevalue, (SELECT count(post_id) FROM vote v WHERE v.value = 1) as upvotes, (SELECT count(post_id) FROM vote v WHERE v.value = -1) as downvotes
       FROM post p
       INNER JOIN user u ON p.fk_owner_user_id = u.id
-      LEFT JOIN vote vv ON ${req.body.currenUserId} = vv.user_id
+      LEFT JOIN (SELECT * FROM vote WHERE user_id = 1) vv ON p.fk_owner_user_id = vv.user_id
       LEFT JOIN vote v ON p.id = v.post_id
+      ${savedFilter}
       ${usernameFilter}
       ${searchFilter}
       GROUP BY p.id
-      ORDER BY p.id 
+      ${order} 
       limit ${req.body.take} 
       offset ${req.body.skip}`, (err, result) => {
+      con.release()
+      if (err) {
+        return res.status(500).json({ err })
+      } else {
+        console.log(result)
+        return res.send(result)
+      }
+    })
+  })
+})
+
+router.post('/vote', (req, res) => {
+  console.log('hola')
+  database.getConnection((_err, con) => {
+    con.query(`
+      INSERT INTO vote (user_id, post_id, value)
+      VALUES(${req.body.userId}, ${req.body.postId}, ${req.body.voteValue})
+      ON DUPLICATE KEY UPDATE value = ${req.body.voteValue}
+      `, (err, result) => {
       con.release()
       if (err) {
         return res.status(500).json({ err })
